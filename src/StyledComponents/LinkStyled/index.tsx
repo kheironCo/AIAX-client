793480420be7246d67d614160efc8c05e605e060
MarcosRehtanz <marcos.rehtanz@gmail.com>
2024-03-17 21:54:42 -0300

    build: install styled-components

---
+++ src/StyledComponents/LinkStyled/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const LinkStyled = styled.a`
+  font-weight: 500;
+  color: #646cff;
+  text-decoration: inherit;
+  :hover {
+    color: #535bf2;
+  }
+`;
+
+export default LinkStyled;
