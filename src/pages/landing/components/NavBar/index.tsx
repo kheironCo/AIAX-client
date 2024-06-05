b8037a80f9f8161e3cf98e8afc334d0f11f52ff7
Gaston Jaurena <gassjau@gmail.com>
2024-06-04 21:52:30 -0300

    NavBar

--- src/pages/landing/components/NavBar/index.tsx
+++ src/pages/landing/components/NavBar/index.tsx
@@ -1,3 +1,26 @@
+import { NavLink } from 'react-router-dom';
+import logo from '../imgs/logo.svg';
+// import "./style.css"
+import { styles } from './styles';
+import { css } from '@emotion/react';
+
 export const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div css={css(styles)}>
+      <nav>
+        <img src={logo} alt="logo" />
+        <div>
+          <div>
+            <NavLink to="#">Contacto</NavLink>
+            <NavLink to="#">Producto</NavLink>
+            <NavLink to="#">Precios</NavLink>
+          </div>
+          <div>
+            <button>Login</button>
+            <button>Register</button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
