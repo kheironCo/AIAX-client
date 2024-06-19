1f0146eb125b18f430f86dcff9af891e286b2351
Gaston Jaurena <gassjau@gmail.com>
2024-06-19 13:21:40 -0300

    Functions English

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,7 @@
 import { NavBar } from 'pages/landing/components';
 import styles from './style.module.css';
 import { ButtonKUI, InputFieldPasswordKUI, InputFieldTextKUI } from 'kheiron-ui';
-import imagenLlave from 'assets/imgs/imagenLlave.png';
+import keyimg from 'assets/imgs/imagenLlave.png';
 
 export const Login = () => {
   return (
@@ -9,20 +9,20 @@ export const Login = () => {
       <NavBar />
       <div className={styles.div}>
         <h2 className={styles.h2}>Login</h2>
-        <div className={styles.contenedor}>
-          <form action="" className={styles.formulario}>
-            <div className={styles.divisorPadding}>
-              <InputFieldTextKUI label="Nombre: "></InputFieldTextKUI>
-              <InputFieldPasswordKUI label="Contraseña:">Contraseña:</InputFieldPasswordKUI>
+        <div className={styles.container}>
+          <form className={styles.form}>
+            <div className={styles.padding}>
+              <InputFieldTextKUI label="Name: "></InputFieldTextKUI>
+              <InputFieldPasswordKUI label="Password:">Contraseña:</InputFieldPasswordKUI>
             </div>
-            <div className={styles.botones}>
-              <ButtonKUI palette="primary">Ingresar</ButtonKUI>
-              <ButtonKUI palette="primary">¿Olvidaste tu contraseña?</ButtonKUI>
+            <div className={styles.groupButtons}>
+              <ButtonKUI palette="primary">Login</ButtonKUI>
+              <ButtonKUI palette="primary">¿Did you forget your password?</ButtonKUI>
             </div>
           </form>
-          <img src={imagenLlave} alt="Imagen de llave" className={styles.imagen} />
+          <img src={keyimg} alt="Imagen de llave" className={styles.imagen} />
         </div>
-        <ButtonKUI palette="gold">Crear cuenta nueva</ButtonKUI>
+        <ButtonKUI palette="gold">Create new account</ButtonKUI>
       </div>
     </>
   );
--- src/pages/landing/components/NavBar/index.tsx
+++ src/pages/landing/components/NavBar/index.tsx
@@ -16,19 +16,19 @@ export const NavBar = () => {
     <div>
       <nav className={styles.nav}>
         <img src={logo} alt="logo" onClick={volver} className={styles.logo} />
-        <div className={styles.divDerechaNav}>
-          <div className={styles.divChicoPadre}>
-            <NavLink to="/contacto" className={styles.divChico}>
-              Contacto
+        <div className={styles.fatherGroupNav}>
+          <div className={styles.groupNav}>
+            <NavLink to="/contacto" className={styles.navLinkStyle}>
+              Contact
             </NavLink>
-            <NavLink to="/producto" className={styles.divChico}>
-              Producto
+            <NavLink to="/producto" className={styles.navLinkStyle}>
+              Product
             </NavLink>
-            <NavLink to="/precios" className={styles.divChico}>
-              Precios
+            <NavLink to="/precios" className={styles.navLinkStyle}>
+              Price
             </NavLink>
           </div>
-          <div className={styles.separar}>
+          <div className={styles.groupButtons}>
             <ButtonKUI palette="gold" onClick={navegar}>
               Login
             </ButtonKUI>
