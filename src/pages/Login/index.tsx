e386c972c43eab169fbda7c106123ec678877b72
Gaston Jaurena <gassjau@gmail.com>
2024-06-29 21:00:31 -0300

    Adding Firebase into Login and Register

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,18 +2,49 @@ import { NavBar } from 'pages/landing/components';
 import styles from './style.module.css';
 import { ButtonKUI, InputFieldPasswordKUI, InputFieldTextKUI } from 'kheiron-ui';
 import keyimg from 'assets/imgs/imagenLlave.png';
+import { useState } from 'react';
+import { login } from '../../config/firebase.js';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+  const handleSubmitLogin = async (e: any) => {
+    e.preventDefault();
+    try {
+      const credencialUser = await login({ email, password });
+      if (credencialUser.operationType == 'signIn') {
+        alert('Ha ingresado correctamente');
+        navigate('/dashboard');
+      }
+    } catch (error) {
+      console.log(error);
+      alert('Error');
+    }
+  };
+
   return (
     <>
       <NavBar />
       <div className={styles.div}>
         <h2 className={styles.h2}>Login</h2>
         <div className={styles.container}>
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleSubmitLogin}>
             <div>
-              <InputFieldTextKUI label="Name: " width="230px"></InputFieldTextKUI>
-              <InputFieldPasswordKUI label="Password:" width="204px"></InputFieldPasswordKUI>
+              <InputFieldTextKUI
+                label="Name: "
+                width="230px"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></InputFieldTextKUI>
+              <InputFieldPasswordKUI
+                label="Password:"
+                width="204px"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+              ></InputFieldPasswordKUI>
             </div>
             <div className={styles.groupButtons}>
               <ButtonKUI palette="primary">Login</ButtonKUI>
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -7,8 +7,46 @@ import {
   InputFieldPasswordKUI,
   InputFieldTextKUI,
 } from 'kheiron-ui';
+import { useState } from 'react';
+import { register } from '../../config/firebase.js';
+import { useNavigate } from 'react-router-dom';
 
 export const Register = () => {
+  const [user, setUser] = useState({
+    name: '',
+    lastName: '',
+    bussines: '',
+    email: '',
+    phone: '',
+    password: '',
+    repeatPassword: '',
+  });
+  const { name, lastName, bussines, email, phone, password, repeatPassword } = user;
+
+  function handleChangeregister(e: any) {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  const navigate = useNavigate();
+
+  const handleSubmitRegister = async (e: any) => {
+    e.preventDefault();
+    try {
+      const credencialUser = await register({ email, password });
+      console.log(credencialUser);
+      if (credencialUser.operationType == 'signIn') {
+        alert('Ha Registrado su cuenta!');
+        navigate('/dashboard');
+      }
+    } catch (error) {
+      console.log(error);
+      alert('Error al crear cuenta');
+    }
+  };
+
   return (
     <>
       <NavBar />
@@ -16,16 +54,56 @@ export const Register = () => {
         <h1 className={styles.h1}>Sign Up</h1>
         <div className={styles.containerform}>
           <img className={styles.img} src={Registerimg} alt="Register image" />
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleSubmitRegister}>
             <div className={styles.inTwoColumns}>
-              <InputFieldTextKUI label="Name" />
-              <InputFieldTextKUI label="Last Name" />
+              <InputFieldTextKUI
+                label="Name"
+                name="name"
+                value={name}
+                onChange={handleChangeregister}
+              />
+              <InputFieldTextKUI
+                label="Last Name"
+                name="lastName"
+                value={lastName}
+                onChange={handleChangeregister}
+              />
             </div>
-            <InputFieldTextKUI label="Business name" width="361px" />
-            <InputFieldTextKUI label="Email" width="361px" />
-            <InputFieldTextKUI label="Phone" width="361px" />
-            <InputFieldPasswordKUI label="Password" width="337px" />
-            <InputFieldPasswordKUI label="Repeat password" width="337px" />
+            <InputFieldTextKUI
+              label="Business name"
+              width="361px"
+              name="bussines"
+              value={bussines}
+              onChange={handleChangeregister}
+            />
+            <InputFieldTextKUI
+              label="Email"
+              value={email}
+              width="361px"
+              name="email"
+              onChange={handleChangeregister}
+            />
+            <InputFieldTextKUI
+              label="Phone"
+              width="361px"
+              name="phone"
+              value={phone}
+              onChange={handleChangeregister}
+            />
+            <InputFieldPasswordKUI
+              label="Password"
+              value={password}
+              width="337px"
+              name="password"
+              onChange={handleChangeregister}
+            />
+            <InputFieldPasswordKUI
+              label="Repeat password"
+              width="337px"
+              value={repeatPassword}
+              name="repeatPassword"
+              onChange={handleChangeregister}
+            />
             <br />
             <div className={styles.organizarCheck}>
               <InputFieldCheckboxKUI label="Accept Kheiron terms and conditions" position="right">
