9282d86a1aa162c1c727daacf2a2e82c1d5c2bc1
Gaston Jaurena <gassjau@gmail.com>
2024-07-02 13:42:11 -0300

    Adding the mix of zod with the firebase login

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -10,23 +10,10 @@ import {
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UserRegister, IUserRegister } from 'schema/User';
-import { ChangeEvent, useState } from 'react';
 import { registerNew } from '../../config/firebase.js';
 import { useNavigate } from 'react-router-dom';
-import { any } from 'zod';
 
 export const Register = () => {
-  const [user, setUser] = useState({
-    name: '',
-    lastName: '',
-    bussinesName: '',
-    email: '',
-    phone: '',
-    password: '',
-    repeatPassword: '',
-    terms: true,
-  });
-
   const {
     register,
     handleSubmit,
@@ -34,7 +21,7 @@ export const Register = () => {
   } = useForm<IUserRegister>({ resolver: zodResolver(UserRegister) });
 
   const handleValidSubmit = async (data: IUserRegister) => {
-    console.log(data);
+    const { email, password } = data;
     try {
       const credencialUser = await registerNew({ email, password });
 
@@ -48,15 +35,6 @@ export const Register = () => {
     }
   };
 
-  const { name, lastName, bussinesName, email, phone, password, repeatPassword, terms } = user;
-
-  function handleChangeRegister(e: ChangeEvent<HTMLInputElement>): void {
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
-  }
-
   const navigate = useNavigate();
 
   return (
@@ -68,61 +46,19 @@ export const Register = () => {
           <img className={styles.img} src={ImageRegister} alt="Register image" />
           <form className={styles.form} onSubmit={handleSubmit(handleValidSubmit)}>
             <div className={styles.inTwoColumns}>
-              <InputFieldTextKUI
-                width="100%"
-                {...register('name')}
-                label="Name"
-                name="name"
-                value={name}
-                onChange={handleChangeRegister}
-              />
-              <InputFieldTextKUI
-                width="100%"
-                {...register('lastName')}
-                label="Last Name"
-                name="lastName"
-                value={lastName}
-                onChange={handleChangeRegister}
-              />
+              <InputFieldTextKUI width="100%" {...register('name')} label="Name" />
+              <InputFieldTextKUI width="100%" {...register('lastName')} label="Last Name" />
             </div>
             {errors.name?.message && <h6 className={styles.error}>{`${errors.name.message}`}</h6>}
-            <InputFieldTextKUI
-              {...register('businessName')}
-              label="Business name"
-              width="100%"
-              name="bussinesName"
-              value={bussinesName}
-              onChange={handleChangeRegister}
-            />
+            <InputFieldTextKUI {...register('businessName')} label="Business name" width="100%" />
             {errors.businessName?.message && (
               <h6 className={styles.error}>{`${errors.businessName.message}`}</h6>
             )}
-            <InputFieldTextKUI
-              {...register('email')}
-              label="Email"
-              width="100%"
-              name="email"
-              value={email}
-              onChange={handleChangeRegister}
-            />
+            <InputFieldTextKUI {...register('email')} label="Email" width="100%" />
             {errors.email?.message && <h6 className={styles.error}>{`${errors.email.message}`}</h6>}
-            <InputFieldTextKUI
-              {...register('phone')}
-              label="Phone"
-              width="100%"
-              name="phone"
-              value={phone}
-              onChange={handleChangeRegister}
-            />
+            <InputFieldTextKUI {...register('phone')} label="Phone" width="100%" />
             {errors.phone?.message && <h6 className={styles.error}>{`${errors.phone.message}`}</h6>}
-            <InputFieldPasswordKUI
-              {...register('password')}
-              label="Password"
-              width="100%"
-              name="password"
-              value={password}
-              onChange={handleChangeRegister}
-            />
+            <InputFieldPasswordKUI {...register('password')} label="Password" width="100%" />
             {errors.password?.message && (
               <h6 className={styles.error}>{`${errors.password.message}`}</h6>
             )}
@@ -130,9 +66,6 @@ export const Register = () => {
               {...register('repeatPassword')}
               label="Repeat password"
               width="100%"
-              name="repeatPassword"
-              value={repeatPassword}
-              onChange={handleChangeRegister}
             />
             {errors.repeatPassword?.message && (
               <h6 className={styles.error}>{`${errors.repeatPassword.message}`}</h6>
